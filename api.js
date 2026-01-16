67eb1239f9060867b22d6194acd94c6ab5b2a3d4
arevas1729 <80892407+arevas1729@users.noreply.github.com>
2026-01-16 21:40:13 +0400

    Create api.js

---
+++ api.js
@@ -0,0 +1,19 @@
+<script>
+const API_URL = "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE";
+
+async function apiPost(action, data = {}) {
+  const token = localStorage.getItem("token");
+  const res = await fetch(API_URL, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ action, token, ...data })
+  });
+  return res.json();
+}
+
+async function apiGet(action) {
+  const token = localStorage.getItem("token");
+  const res = await fetch(`${API_URL}?action=${action}&token=${token}`);
+  return res.json();
+}
+</script>
